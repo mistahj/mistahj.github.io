a1fc929ed52bec59a0235b10fc98a6f180e0d877
Justin <mistahj@gmail.com>
2013-07-30 18:54:27 -0700

    Updates to final project
    Added new css and gallery folder for images

--- final/js/app.js
+++ final/js/app.js
@@ -23,11 +23,11 @@ $(document).ready(function(){
 		$('a#photography').addClass('active')
 	})
 
-	$('a.nav').click(function(){
-		$('a.nav').css('opacity', '1') 
-		$('a.nav').css('color', '#021B4B')
-		$('a.nav:not(.active)').fadeTo(500, 0.5)
-		$('a.nav:not(.active)').css('color', '#0FA5DD')	
+	$('a.topnav').click(function(){
+		$('a.topnav').css('opacity', '1') 
+		$('a.topnav').css('color', '#021B4B')
+		$('a.topnav:not(.active)').fadeTo(500, 0.5)
+		$('a.topnav:not(.active)').css('color', '#0FA5DD')	
 	})
 
 	$('a#outdoors').mouseover(function(){
@@ -46,9 +46,28 @@ $(document).ready(function(){
 		$('a#black').addClass('actived')
 	})
 
-	$('a#poem').mouseover(function(){
-		$('a').removeClass('actived')
+	$('a#poem').hover(function(){
 		$('a#poem').addClass('actived')
+	},
+	function(){ $('a#poem').removeClass('actived')
+	})
+
+	$('a#black').hover(function(){
+		$('a#black').addClass('actived')
+	},
+	function(){ $('a#black').removeClass('actived')
+	})
+
+	$('a#hoopin').hover(function(){
+		$('a#hoopin').addClass('actived')
+	},
+	function(){ $('a#hoopin').removeClass('actived')
+	})
+
+	$('a#outdoors').hover(function(){
+		$('a#outdoors').addClass('actived')
+	},
+	function(){ $('a#outdoors').removeClass('actived')
 	})
 
 	$('a.sidenav').click(function(){
@@ -56,10 +75,37 @@ $(document).ready(function(){
 		$('a.sidenav').css('color', '#021B4B')
 		$('a.sidenav:not(.actived)').css('background-color', '#0C394F')
 		$('a.sidenav:not(.actived)').css('color', '#ffffff')
+		
 	})
 
 
+	$('.photo').hover(function(){
+		$(this).find('.caption').slideDown(400);
+	},
+	function(){
+		$(this).find('.caption').fadeOut(300);
+	})
 
+
+	$('#hoopin').click(function(){
+		$('.outdoors').fadeOut(),
+		$('.hoopin').slideUp().fadeIn();
+		$('.black').fadeOut();
+	})
+
+	$('#outdoors').click(function(){
+		$('.outdoors').fadeIn();
+		$('.hoopin').fadeOut();
+		$('.black').fadeOut();
+
+	})
+
+	$('#black').click(function(){
+		$('.hoopin').fadeOut();
+		$('.black').fadeIn();
+		$('.outdoors').fadeOut();
+
+	})
 
 
 
