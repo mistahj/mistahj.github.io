8426fc76240195965d7b00f8e54b9f7f319cdbf2
Justin Smith <mistahj@gmail.com>
2014-04-27 11:47:13 -0700

    updated functionality of jstest
    Fixed issue with quickly disappearing text with result of rock, paper,
    scissors

--- jstest/js/app2.js
+++ jstest/js/app2.js
@@ -1,57 +1,50 @@
 // Your JS code goes here.
 $(document).ready(function(){
 
-$("form").submit(function(){
-   var userInput = $("#move").val();
-   var compInput = Math.floor((Math.random() * 15)+1);  
 
-    if((userInput !== "scissors") && (userInput !== "rock") && (userInput   
-    !== "paper")){
-      $('.words').text("please enter rock, paper, or scissors").delay(2000)
-    };
-   /* }else if (mrmid == "scissors") {
-     $('.words').text("you picked scissors")
-    }else if (mrmid == "paper") {
-     $('.words').text("you picked paper")
-    }else if (mrmid == "rock") {
-     $('.words').text("you picked rock")
-    };*/
-  
-    if((compInput > 0) && (compInput <= 5)){
-	 compInput = "rock";
-	 console.log(compInput);
-}else if((compInput >= 6) && (compInput <= 10)){
-	compInput = "paper";
-	 console.log(compInput);
-}else if((compInput >= 11) && (compInput <= 15)){
-	 compInput = "scissors";
-	 console.log(compInput);
-};
-  
-if(compInput === userInput){
-  $('.words').text("Computer threw" + " " + compInput + "." + " It's a tie!");
-} else if(userInput === "rock") {
-	    if(compInput === "scissors"){
-	        $('.words').after("Computer threw" + " " + compInput + "." + " rock beats scissors, you win!");
-      } else if(compInput === "paper"){
-    $('.words').after("Computer threw" + " " + compInput + "." + " paper beats rock, you lose.");
-	}
-}else if(userInput === "paper"){
-	if(compInput === "rock"){
-		$('.words').text("Computer threw" + " " + compInput + "." + " paper beats rock, you win!");
-	}else if(compInput === "scissors"){
-		$('.words').text("Computer threw" + " " + compInput + "." + " scissors beat paper, you lose");
-	}
-}else if(userInput === "scissors"){
-	if(compInput === "paper"){
-		$('.words').text("Computer threw" + " " + compInput + "." + " scissors beat paper, you win!");
-	}else if(compInput === "rock"){
-		$('.words').text("Computer threw" + " " + compInput + "." + " rock beats scissors, you lose.");
-	}
-};
+    $(".button").one('click', function () {
+        var userInput = $("#move").val();
+        var compInput = Math.floor((Math.random() * 15) + 1);
 
+        if ((userInput !== "scissors") && (userInput !== "rock") && (userInput !== "paper")) {
+            $('.words').append("<p>please enter rock, paper, or scissors</p>");
+        }
+
+        if ((compInput > 0) && (compInput <= 5)) {
+            compInput = "rock";
+            console.log(compInput);
+        } else if ((compInput >= 6) && (compInput <= 10)) {
+            compInput = "paper";
+            console.log(compInput);
+        } else if ((compInput >= 11) && (compInput <= 15)) {
+            compInput = "scissors";
+            console.log(compInput);
+        }
+
+        if (compInput === userInput) {
+            $('.words').append('<p>Computer threw' + " " + compInput + "." + " It's a tie!</p>");
+        } else if (userInput === "rock") {
+            if (compInput === "scissors") {
+                $('.words').append("<p>Computer threw" + " " + compInput + "." + " rock beats scissors, you win!</p>");
+            } else if (compInput === "paper") {
+                $('.words').append("<p>Computer threw" + " " + compInput + "." + " paper beats rock, you lose.</p>");
+            }
+        } else if (userInput === "paper") {
+            if (compInput === "rock") {
+                $('.words').append("<p>Computer threw" + " " + compInput + "." + " paper beats rock, you win!</p>");
+            } else if (compInput === "scissors") {
+                $('.words').append("<p>Computer threw" + " " + compInput + "." + " scissors beat paper, you lose.</p>");
+            }
+        } else if (userInput === "scissors") {
+            if (compInput === "paper") {
+                $('.words').append("<p>Computer threw" + " " + compInput + "." + " scissors beat paper, you win!</p>");
+            } else if (compInput === "rock") {
+                $('.words').append("<p>Computer threw" + " " + compInput + "." + " rock beats scissors, you lose.</p>");
+            }
+        }
+
+
+            $(".button").hide();
+            $("#reset").show();
 });
-
-
-
 });
